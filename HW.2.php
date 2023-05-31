69386c75dec4d3b2819ae3ad37725f53c0422613
HAMID1255 <129625837+HAMID1255@users.noreply.github.com>
2023-05-31 18:18:33 +0300

    Add files via upload

---
+++ HW.2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homework</title>
+    <style>
+      body {
+			background-color: red;
+   
+		}
+
+		body {
+			background-color: lightblue;
+          }
+		   img{
+        width: 350px;
+        height: 200px;
+      }
+      
+		
+    
+	</style>
+  </head>
+  <body>
+    <h1>homework2</h1>
+    <p>Images and Image Maps</p> 
+<img src="https://images.theconversation.com/files/236859/original/file-20180918-158240-1jd9gm6.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip" alt="Example Image" usemap="#example-map">
+<figcaption>A description of the example image.</figcaption>
+<map name="example-map">
+  <area shape="rect" coords="0,50,100,100" href="page1.html" alt = "page" >
+  <area shape="circle" coords="100,200,50" href="page2.html">
+</map>
+
+
+  </body>
+</html>
